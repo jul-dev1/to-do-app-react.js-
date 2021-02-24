2b8e529384158ecc33f3bc16ac874db652471ec8
Jul <be;;_r9@yahoo.com>
2021-02-24 16:28:11 +0200

    2 commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Todos from './components/Todos'
 function App() {
   return (
     <div className="header">
-      <h1>To Do App</h1>
+      <h1>To Do App👍</h1>
       <Todos />
     </div>
   );
